7f664ad1ec851ef0ddc53715e71b214649fe6779
Mohammed A Sallout <sallout.81995@gmail.com>
2020-12-11 02:33:04 +0200

    get time ago

---
+++ GetTime/src/main/java/com/mas/gettime/GetTime.java
@@ -0,0 +1,122 @@
+package com.mas.gettime;
+
+import android.content.Context;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class GetTime {
+
+    private static final int SECOND_MILLIS = 1000;
+    private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+    private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+    private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
+
+
+    public static String getTimeAgo(Context context, String timeString, DateFormat dateFormat, int server_millis_time_zone) {
+        //2020-11-24T22:17:20
+        String agoTime = "";
+
+        String str_date = timeString.replace(".000000Z", "");
+
+        // DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        Date date = null;
+        try {
+            date = (Date) dateFormat.parse(str_date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        // =============================================
+        Calendar currentCalender = Calendar.getInstance();
+        TimeZone tz = currentCalender.getTimeZone();
+        int zoneTime = (tz.getRawOffset());
+
+        long now = currentCalender.getTimeInMillis();
+
+        Calendar postCalender = Calendar.getInstance();
+        postCalender.setTime(date);
+        long time;
+
+        if (zoneTime == server_millis_time_zone) {
+            time = date.getTime();
+        } else {
+            time = date.getTime() + zoneTime;
+        }
+
+        // TODO: localize
+        final long diff = now - time;
+
+
+        //==============================================
+
+        //return PrettyTimeAgo.getTimeAgo(date.getTime() + zoneTime);
+
+        return getTheTime(diff, context, time);
+
+    }
+
+    public static String getTimeAgo(Context context, Long millis_time, int server_millis_time_zone) {
+        //2020-11-24T22:17:20
+        // =============================================
+
+        Calendar currentCalender = Calendar.getInstance();
+        TimeZone tz = currentCalender.getTimeZone();
+        int zoneTime = (tz.getRawOffset());
+
+        long now = currentCalender.getTimeInMillis();
+        long time;
+        if (zoneTime == server_millis_time_zone) {
+            time = millis_time;
+        } else {
+            time = millis_time + zoneTime;
+
+        }
+
+
+        // TODO: localize
+        final long diff = now - time;
+
+        //==============================================
+
+        //return PrettyTimeAgo.getTimeAgo(date.getTime() + zoneTime);
+
+        return getTheTime(diff, context, time);
+
+    }
+
+    private static String getTheTime(long diff, Context context, Long time) {
+        if (diff < MINUTE_MILLIS) {
+            return context.getString(R.string.just_now);
+        } else if (diff < 2 * MINUTE_MILLIS) {
+            return "a minute ago";
+        } else if (diff < 50 * MINUTE_MILLIS) {
+            return diff / MINUTE_MILLIS + " " + context.getString(R.string.minutes_ago);
+        } else if (diff < 90 * MINUTE_MILLIS) {
+            return context.getString(R.string.an_hour_ago);
+        } else if (diff < 24 * HOUR_MILLIS) {
+            return diff / HOUR_MILLIS + " " + context.getString(R.string.an_hour_ago);
+        } else if (diff < 48 * HOUR_MILLIS) {
+            return context.getString(R.string.yesterday);
+        } else {
+            if (diff / DAY_MILLIS <= 5) {
+                return diff / DAY_MILLIS + " " + context.getString(R.string.days_ago);
+            } else {
+                return getDateString(time) + "";
+            }
+        }
+    }
+
+    private static String getDateString(long time) {
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(time);
+        Date d = c.getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH);
+        return sdf.format(d);
+    }
+}
---
+++ app/src/main/java/com/mas/androidtimeago/MainActivity.java
@@ -0,0 +1,59 @@
+package com.mas.androidtimeago;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.mas.gettime.GetTime;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        // current time when test is :2020-12-09T21:03:32.000000Z
+
+        String just_now = "2020-12-09T21:03:32.000000Z";
+        String a_minute_ago = "2020-12-09T21:02:32.000000Z";
+        String before_x_min = "2020-12-09T20:55:32.000000Z";
+        String before_1_Hour = "2020-12-09T19:59:32.000000Z";
+        String before_1_day = "2020-12-08T20:53:09.000000Z";
+        String before_2_day = "2020-12-06T22:53:09.000000Z";
+        String more_5_days = "2020-11-25T03:09:09.000000Z";
+
+
+
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.ENGLISH);
+
+
+        // the server millis time zone is the server time zone it will be positive or negative
+        // If server zone time is +2 then the he server millis time zone = 2h*60m*60s*10000 =  7200000
+        //If server zone time is -2 then the he server millis time zone = -2h*60m*60s*10000 = -7200000
+
+
+        // the time as text
+        Log.e("MAS_TAG","just_now     : "+ GetTime.getTimeAgo(MainActivity.this, just_now, formatter,0));
+        Log.e("MAS_TAG","a minute ago : "+ GetTime.getTimeAgo(MainActivity.this, a_minute_ago, formatter ,0));
+        Log.e("MAS_TAG","before_x_min : "+ GetTime.getTimeAgo(MainActivity.this, before_x_min, formatter,0));
+        Log.e("MAS_TAG","before_1_Hour: "+ GetTime.getTimeAgo(MainActivity.this, before_1_Hour, formatter,0));
+        Log.e("MAS_TAG","before_1_day : "+ GetTime.getTimeAgo(MainActivity.this, before_1_day, formatter,0));
+        Log.e("MAS_TAG","before_2_day : "+ GetTime.getTimeAgo(MainActivity.this, before_2_day, formatter,0));
+        Log.e("MAS_TAG","more 5 days  : "+ GetTime.getTimeAgo(MainActivity.this, more_5_days, formatter,0));
+
+
+
+        // if you want to use time in millis
+        Log.e("MAS_TAG","just_now   ts  : "+ GetTime.getTimeAgo(MainActivity.this, 1607535180000L , 0));
+
+
+    }
+}
